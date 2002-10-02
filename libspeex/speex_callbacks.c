402f2bcb6e9a98581dcd6a67815bde6cb680a34c
jmvalin <jmvalin@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2002-10-02 19:52:53 +0000

    adding in-band signalling and callback handling
    git-svn-id: http://svn.xiph.org/trunk/speex@3968 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ libspeex/speex_callbacks.c
@@ -0,0 +1,38 @@
+/* Copyright (C) 2002 Jean-Marc Valin
+   File speex_callbacks.c
+   Callback handling and in-band signalling
+
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include "speex_callbacks.h"
+
+int speex_inband_handler(void *state, void *data)
+{
+}
+
+int speex_std_mode_request_handler(void *state, void *data)
+{
+}
+
+int speex_std_high_mode_request_handler(void *state, void *data)
+{
+}
+
+int speex_std_char_handler(void *state, void *data)
+{
+}
---
+++ libspeex/speex_callbacks.h
@@ -0,0 +1,58 @@
+/* Copyright (C) 2002 Jean-Marc Valin*/
+/**
+  @file speex_callbacks.h
+  @brief Describes callback handling and in-band signalling
+*/
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#ifndef SPEEX_CALLBACKS_H
+#define SPEEX_CALLBACKS_H
+
+#include "speex.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SPEEX_INBAND_MODE_REQUEST        0
+#define SPEEX_INBAND_HIGH_MODE_REQUEST   1
+#define SPEEX_INBAND_CHAR                2
+
+
+typedef int (*speex_callback_func)(void *state, void *data);
+
+typedef struct SpeexCallback {
+   int callback_id;
+   speex_callback_func func;
+   void *data;
+} SpeexCallback;
+
+int speex_inband_handler(void *state, void *data);
+
+int speex_std_mode_request_handler(void *state, void *data);
+
+int speex_std_high_mode_request_handler(void *state, void *data);
+
+int speex_std_char_handler(void *state, void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
