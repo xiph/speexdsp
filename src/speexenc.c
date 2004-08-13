f778fb7e644d2a80c1e2b871b4a899b1509e0df8
conrad <conrad@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-08-13 07:33:20 +0000

    speex_get_mode() -> speex_lib_get_mode() ohh beer!
    git-svn-id: http://svn.xiph.org/trunk/speex@7557 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/speexenc.c
+++ src/speexenc.c
@@ -537,7 +537,7 @@ int main(int argc, char **argv)
       if (rate!=8000 && rate!=16000 && rate!=32000)
          fprintf (stderr, "Warning: Speex is only optimized for 8, 16 and 32 kHz. It will still work at %d Hz but your mileage may vary\n", rate); 
 
-   mode = speex_get_mode (modeID);
+   mode = speex_lib_get_mode (modeID);
 
    speex_init_header(&header, rate, 1, mode);
    header.frames_per_packet=nframes;
