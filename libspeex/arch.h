b50c2060a97dab5977f96ef21540861f619c84fe
jm <jm@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-04-02 04:10:20 +0000

    Oops, forgot to add MAC16_16_Q13 for float too.
    git-svn-id: http://svn.xiph.org/trunk/speex@9104 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- libspeex/arch.h
+++ libspeex/arch.h
@@ -125,6 +125,7 @@ typedef float spx_word64_t;
 #define MAC16_32_Q15(c,a,b)     ((c)+(a)*(b))
 
 #define MAC16_16_Q11(c,a,b)     ((c)+(a)*(b))
+#define MAC16_16_Q13(c,a,b)     ((c)+(a)*(b))
 #define MULT16_16_Q11(a,b)     ((a)*(b))
 #define MULT16_16_Q13(a,b)     ((a)*(b))
 #define MULT16_16_Q14(a,b)     ((a)*(b))
