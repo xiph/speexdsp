7a22cd7fa62695f9f30ee12cc041b9383600076a
jm <jm@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-06-06 05:22:32 +0000

    Special case for complexity 1
    git-svn-id: http://svn.xiph.org/trunk/speex@9367 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- libspeex/vq_bfin.h
+++ libspeex/vq_bfin.h
@@ -34,6 +34,37 @@
 #define OVERRIDE_VQ_NBEST
 void vq_nbest(spx_word16_t *in, const spx_word16_t *codebook, int len, int entries, spx_word32_t *E, int N, int *nbest, spx_word32_t *best_dist, char *stack)
 {
+   if (N==1)
+   {
+      best_dist[0] = 2147483647;
+      spx_word32_t dist;
+      __asm__ __volatile__
+         (
+            "LC0 = %8;\n\t"
+            "R2 = 0;\n\t"
+            "LOOP entries_loop%= LC0;\n\t"
+            "LOOP_BEGIN entries_loop%=;\n\t"
+               "%0 = [%4++];\n\t"
+               "%0 >>= 1;\n\t"
+               "A0 = %0;\n\t"
+               "I0 = %6;\n\t"
+               "L0 = 0;\n\t"
+               "R0.L = W[%1++%7] || R1.L = W[I0++];\n\t"
+               "LOOP vq_loop%= LC0 = %5;\n\t"
+               "LOOP_BEGIN vq_loop%=;\n\t"
+                  "%0 = (A0 -= R0.L*R1.L) (IS) || R0.L = W[%1++%7] || R1.L = W[I0++];\n\t"
+               "LOOP_END vq_loop%=;\n\t"
+               "%0 = (A0 -= R0.L*R1.L) (IS);\n\t"
+               "cc = %0 < %2;\n\t"
+               "if cc %2 = %0;\n\t"
+               "if cc %3 = R2;\n\t"
+               "R2 += 1;\n\t"
+            "LOOP_END entries_loop%=;\n\t"
+            : "=D" (dist), "=a" (codebook), "=d" (best_dist[0]), "=d" (nbest[0]), "=a" (E)
+            : "a" (len-1), "a" (in), "a" (2), "d" (entries), "1" (codebook), "4" (E), "2" (best_dist[0]), "3" (nbest[0])
+            : "R0", "R1", "R2", "I0", "L0", "A0", "cc", "memory"
+         );
+   } else {
    int i,k,used;
    used = 0;
    for (i=0;i<entries;i++)
@@ -67,5 +98,5 @@ void vq_nbest(spx_word16_t *in, const spx_word16_t *codebook, int len, int entri
          used++;
       }
    }
-
+   }
 }
