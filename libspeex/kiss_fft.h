3aa5b2da3dfee9baa337e30e025b6d651c554f6c
jm <jm@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-12-22 11:16:04 +0000

    some bugfixes
    git-svn-id: http://svn.xiph.org/trunk/speex@10674 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- libspeex/kiss_fft.h
+++ libspeex/kiss_fft.h
@@ -35,11 +35,7 @@ extern "C" {
 
 #ifdef FIXED_POINT
 #include <sys/types.h>	
-# if (FIXED_POINT == 32)
-#  define kiss_fft_scalar int32_t
-# else	
 #  define kiss_fft_scalar int16_t
-# endif
 #else
 # ifndef kiss_fft_scalar
 /*  default is float */
