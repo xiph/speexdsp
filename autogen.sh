9f5f1e2077d12691d1aad700a13614c46bc75d21
giles <giles@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2006-10-26 15:36:16 +0000

    Merge updates to the autogen.sh script from other packages. Looks for higher version aclocal and automake executables in case the requested isn't available. Reorder the call to libtoolize.
    git-svn-id: http://svn.xiph.org/trunk/speex@11953 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- autogen.sh
+++ autogen.sh
@@ -25,20 +25,25 @@ VERSIONMKINT="sed -e s/[^0-9]//"
                                                                                 
 # do we need automake?
 if test -r Makefile.am; then
-  AM_NEEDED=`fgrep AUTOMAKE_OPTIONS Makefile.am | $VERSIONGREP`
+  AM_OPTIONS=`fgrep AUTOMAKE_OPTIONS Makefile.am`
+  AM_NEEDED=`echo $AM_OPTIONS | $VERSIONGREP`
+  if test x"$AM_NEEDED" = "x$AM_OPTIONS"; then
+    AM_NEEDED=""
+  fi
   if test -z $AM_NEEDED; then
     echo -n "checking for automake... "
     AUTOMAKE=automake
     ACLOCAL=aclocal
     if ($AUTOMAKE --version < /dev/null > /dev/null 2>&1); then
+      echo "yes"
+    else
       echo "no"
       AUTOMAKE=
-    else
-      echo "yes"
     fi
   else
     echo -n "checking for automake $AM_NEEDED or later... "
-    for am in automake-$AM_NEEDED automake$AM_NEEDED automake; do
+    for am in automake-$AM_NEEDED automake$AM_NEEDED \
+	automake automake-1.7 automake-1.8 automake-1.9; do
       ($am --version < /dev/null > /dev/null 2>&1) || continue
       ver=`$am --version < /dev/null | head -n 1 | $VERSIONGREP | $VERSIONMKINT`
       verneeded=`echo $AM_NEEDED | $VERSIONMKINT`
@@ -50,7 +55,8 @@ if test -r Makefile.am; then
     done
     test -z $AUTOMAKE &&  echo "no"
     echo -n "checking for aclocal $AM_NEEDED or later... "
-    for ac in aclocal-$AM_NEEDED aclocal$AM_NEEDED aclocal; do
+    for ac in aclocal-$AM_NEEDED aclocal$AM_NEEDED \
+	aclocal aclocal-1.7 aclocal-1.8 aclocal-1.9; do
       ($ac --version < /dev/null > /dev/null 2>&1) || continue
       ver=`$ac --version < /dev/null | head -n 1 | $VERSIONGREP | $VERSIONMKINT`
       verneeded=`echo $AM_NEEDED | $VERSIONMKINT`
@@ -103,10 +109,10 @@ echo "Generating configuration files for $package, please wait...."
 
 echo "  $ACLOCAL $ACLOCAL_FLAGS"
 $ACLOCAL $ACLOCAL_FLAGS || exit 1
-echo "  autoheader"
-autoheader || exit 1
 echo "  $LIBTOOLIZE --automake"
 $LIBTOOLIZE --automake || exit 1
+echo "  autoheader"
+autoheader || exit 1
 echo "  $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
 $AUTOMAKE --add-missing $AUTOMAKE_FLAGS || exit 1
 echo "  autoconf"
