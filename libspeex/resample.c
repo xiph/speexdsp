1e2f996684671e0248429e3ddf55e9c1b02c60a6
jm <jm@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-08-11 13:18:56 +0000

    Fixed a resampler regression cause by signed/unsigned errors. Also started documenting regressions.
    git-svn-id: http://svn.xiph.org/trunk/speex@13535 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- libspeex/resample.c
+++ libspeex/resample.c
@@ -577,10 +577,10 @@ static void update_filter(SpeexResamplerState *st)
       }
       for (i=0;i<st->den_rate;i++)
       {
-         spx_uint32_t j;
+         spx_int32_t j;
          for (j=0;j<st->filt_len;j++)
          {
-            st->sinc_table[i*st->filt_len+j] = sinc(st->cutoff,((j-st->filt_len/2+1)-((float)i)/st->den_rate), st->filt_len, quality_map[st->quality].window_func);
+            st->sinc_table[i*st->filt_len+j] = sinc(st->cutoff,((j-(spx_int32_t)st->filt_len/2+1)-((float)i)/st->den_rate), st->filt_len, quality_map[st->quality].window_func);
          }
       }
 #ifdef FIXED_POINT
