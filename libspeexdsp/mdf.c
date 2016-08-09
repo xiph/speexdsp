e5ea11d66ff1d32b306bbb7d0b393fce3c4f41cb
Tristan Matthews <tmatth@videolan.org>
2016-08-09 08:55:14 -0400

    mdf: drop duplicate definition of WORD2INT

--- libspeexdsp/mdf.c
+++ libspeexdsp/mdf.c
@@ -88,12 +88,6 @@
 #define WEIGHT_SHIFT 0
 #endif
 
-#ifdef FIXED_POINT
-#define WORD2INT(x) ((x) < -32767 ? -32768 : ((x) > 32766 ? 32767 : (x)))
-#else
-#define WORD2INT(x) ((x) < -32767.5f ? -32768 : ((x) > 32766.5f ? 32767 : floor(.5+(x))))
-#endif
-
 /* If enabled, the AEC will use a foreground filter and a background filter to be more robust to double-talk
    and difficult signals in general. The cost is an extra FFT and a matrix-vector multiply */
 #define TWO_PATH
