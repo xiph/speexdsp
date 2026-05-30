0ae71963ff5e9c2574f7e63e8e68d23b73ddb7bf
Tyler Kim <tkim@xiph.org>
2026-05-29 21:36:28 -0400

    Add interpolate_product_single test

--- tests/checkasm/resample/inner_product_double.c
+++ tests/checkasm/resample/inner_product_double.c
@@ -24,39 +24,44 @@ void test_inner_product_double(void)
     resample_fill_float(a, BUF_SIZE);
     resample_fill_float(b, BUF_SIZE);
 
-    const unsigned len = 256;
+    /* Sweep two filter lengths so the asm gets exercised at multiple sizes
+     * (both are multiples of 8, the SSE2 loop's vector stride). */
+    static const unsigned lens[] = { 128, 256 };
+    for (unsigned li = 0; li < sizeof(lens)/sizeof(lens[0]); li++) {
+        const unsigned len = lens[li];
 
-    if (checkasm_check_func(inner_product_double_c,
-                            "inner_product_double_len%u", len))
-        checkasm_bench_new(a, b, len);
+        if (checkasm_check_func(inner_product_double_c,
+                                "inner_product_double_len%u", len))
+            checkasm_bench_new(a, b, len);
 
 #ifdef USE_SSE2
-    if (active_flags & SPEEXDSP_CPU_FLAG_SSE2) {
-        if (checkasm_check_func(inner_product_double_sse2,
-                                "inner_product_double_len%u", len)) {
-            out_t ref = checkasm_call_ref(a, b, len);
-            out_t res = checkasm_call_new(a, b, len);
-            double scale = resample_abs_dot_scale(a, b, len);
-            if (!is_resample_result_within_tolerance_double(ref, res, scale, len))
-                checkasm_fail();
-            checkasm_bench_new(a, b, len);
+        if (active_flags & SPEEXDSP_CPU_FLAG_SSE2) {
+            if (checkasm_check_func(inner_product_double_sse2,
+                                    "inner_product_double_len%u", len)) {
+                out_t ref = checkasm_call_ref(a, b, len);
+                out_t res = checkasm_call_new(a, b, len);
+                double scale = resample_abs_dot_scale(a, b, len);
+                if (!is_resample_result_within_tolerance_double(ref, res, scale, len))
+                    checkasm_fail();
+                checkasm_bench_new(a, b, len);
+            }
         }
-    }
 #endif
 
 #ifdef HAVE_NEON_INNER_PRODUCT_DOUBLE
-    if (active_flags & SPEEXDSP_CPU_FLAG_NEON) {
-        if (checkasm_check_func(inner_product_double_neon,
-                                "inner_product_double_len%u", len)) {
-            out_t ref = checkasm_call_ref(a, b, len);
-            out_t res = checkasm_call_new(a, b, len);
-            double scale = resample_abs_dot_scale(a, b, len);
-            if (!is_resample_result_within_tolerance_double(ref, res, scale, len))
-                checkasm_fail();
-            checkasm_bench_new(a, b, len);
+        if (active_flags & SPEEXDSP_CPU_FLAG_NEON) {
+            if (checkasm_check_func(inner_product_double_neon,
+                                    "inner_product_double_len%u", len)) {
+                out_t ref = checkasm_call_ref(a, b, len);
+                out_t res = checkasm_call_new(a, b, len);
+                double scale = resample_abs_dot_scale(a, b, len);
+                if (!is_resample_result_within_tolerance_double(ref, res, scale, len))
+                    checkasm_fail();
+                checkasm_bench_new(a, b, len);
+            }
         }
-    }
 #endif
+    }
 
     checkasm_report("inner_product_double");
 #endif
--- tests/checkasm/resample/interpolate_product_single.c
+++ tests/checkasm/resample/interpolate_product_single.c
@@ -24,12 +24,14 @@ void test_interpolate_product_single(void)
                      spx_uint32_t, const in_t *);
 
     /* a: filter input window;  b: sinc table (indexed b[j*oversample + 0..3]).
-     * For len=256, oversample=8 the sinc table needs >= 2052 elements. */
+     * Buffers are sized for the longest length swept below; shorter lengths
+     * read a strict subset. For len=256, oversample=8 the sinc table needs
+     * >= 2052 elements. */
     enum {
-        LEN        = 256,
+        MAX_LEN    = 256,
         OVERSAMPLE = 8,
-        BUF_A_SIZE = LEN,
-        BUF_B_SIZE = LEN * OVERSAMPLE + 4,
+        BUF_A_SIZE = MAX_LEN,
+        BUF_B_SIZE = MAX_LEN * OVERSAMPLE + 4,
     };
     CHECKASM_ALIGN(in_t a[BUF_A_SIZE]);
     CHECKASM_ALIGN(in_t b[BUF_B_SIZE]);
@@ -48,42 +50,49 @@ void test_interpolate_product_single(void)
     const in_t interp[4] = { -0.0625f, 0.5625f, 0.5625f, -0.0625f };
 #endif
 
-    if (checkasm_check_func(interpolate_product_single_c,
-                            "interpolate_product_single_len%u", LEN))
-        checkasm_bench_new(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
+    /* Sweep two filter lengths so the asm gets exercised at multiple sizes.
+     * Both are even (the SSE loop steps i+=2). */
+    static const unsigned lens[] = { 128, 256 };
+    for (unsigned li = 0; li < sizeof(lens)/sizeof(lens[0]); li++) {
+        const unsigned len = lens[li];
+
+        if (checkasm_check_func(interpolate_product_single_c,
+                                "interpolate_product_single_len%u", len))
+            checkasm_bench_new(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
 
 #if defined(USE_SSE) && !defined(FIXED_POINT)
-    if (active_flags & SPEEXDSP_CPU_FLAG_SSE) {
-        if (checkasm_check_func(interpolate_product_single_sse,
-                                "interpolate_product_single_len%u", LEN)) {
-            out_t ref = checkasm_call_ref(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
-            out_t res = checkasm_call_new(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
-            double scale = resample_abs_interp_scale(a, b, LEN, OVERSAMPLE, interp);
-            if (!is_resample_result_within_tolerance_float(ref, res, scale, LEN))
-                checkasm_fail();
-            checkasm_bench_new(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
+        if (active_flags & SPEEXDSP_CPU_FLAG_SSE) {
+            if (checkasm_check_func(interpolate_product_single_sse,
+                                    "interpolate_product_single_len%u", len)) {
+                out_t ref = checkasm_call_ref(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
+                out_t res = checkasm_call_new(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
+                double scale = resample_abs_interp_scale(a, b, len, OVERSAMPLE, interp);
+                if (!is_resample_result_within_tolerance_float(ref, res, scale, len))
+                    checkasm_fail();
+                checkasm_bench_new(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
+            }
         }
-    }
 #endif
 
 #ifdef HAVE_NEON_INTERPOLATE_PRODUCT_SINGLE
-    if (active_flags & SPEEXDSP_CPU_FLAG_NEON) {
-        if (checkasm_check_func(interpolate_product_single_neon,
-                                "interpolate_product_single_len%u", LEN)) {
-            out_t ref = checkasm_call_ref(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
-            out_t res = checkasm_call_new(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
+        if (active_flags & SPEEXDSP_CPU_FLAG_NEON) {
+            if (checkasm_check_func(interpolate_product_single_neon,
+                                    "interpolate_product_single_len%u", len)) {
+                out_t ref = checkasm_call_ref(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
+                out_t res = checkasm_call_new(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
 #ifdef FIXED_POINT
-            if (!is_resample_result_within_tolerance_word16(ref, res, LEN))
-                checkasm_fail();
+                if (!is_resample_result_within_tolerance_word16(ref, res, len))
+                    checkasm_fail();
 #else
-            double scale = resample_abs_interp_scale(a, b, LEN, OVERSAMPLE, interp);
-            if (!is_resample_result_within_tolerance_float(ref, res, scale, LEN))
-                checkasm_fail();
+                double scale = resample_abs_interp_scale(a, b, len, OVERSAMPLE, interp);
+                if (!is_resample_result_within_tolerance_float(ref, res, scale, len))
+                    checkasm_fail();
 #endif
-            checkasm_bench_new(a, b, LEN, (spx_uint32_t)OVERSAMPLE, interp);
+                checkasm_bench_new(a, b, len, (spx_uint32_t)OVERSAMPLE, interp);
+            }
         }
-    }
 #endif
+    }
 
     checkasm_report("interpolate_product_single");
 #endif
